f02f6e1ae4a7be0c9f34ebce08adda9215dfabf1
unknown <aowtearfa@gmail.com>
2020-08-29 19:36:37 +0530

    Resolved errors

--- PersonInfo.java
+++ PersonInfo.java
@@ -1,15 +1,16 @@
- class PersonInfo {
- 	String name;
- 	String address;
- 	String phoneNum;
- 	//parameterized constructor
- 	public void PersonInfo(String n, String a, String p){
- 	name = n;
- 	address = a;
- 	phoneNum = p;
+class PersonInfo {
+        String name;
+        String address;
+        String phoneNum;
+        //parameterized constructor
+        public PersonInfo(String n, String a, String p){
+        name = n;
+        address = a;
+        phoneNum = p;
+	print(); 
+}
+        //method for displaying person record on GUI
+        public void print() {
+                System.out.println("Name: "+ name + " Address: "+ address + " Phone no: "+phoneNum);
  }
- 	//method for displaying person record on GUI
- 	public void print() {
- 		System.out.println("Name: "+ name + "Address: "+ address + "Phone no: "+phoneNum);	
  }
- } 
